78f56416e16553aeba331ccc178eaeea46e04f90
Hisham Bokhari <hish.b92@gmail.com>
2020-01-27 16:48:56 +0000

    Adding deposits, displays  float of any number

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,8 @@ require 'account'
 describe '#deposit_display' do
   it 'displays a given amount in the correct format' do
     expect(deposit_display(1000)).to eq 1000.00
+    expect(deposit_display(2000)).to eq 2000.00
+    expect(deposit_display(3000)).to eq 3000.00
   end
 end
 
