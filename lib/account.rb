b53ba63b307cf5f7a5a7eb3835e5085f883c0673
Hisham Bokhari <hish.b92@gmail.com>
2020-01-27 16:45:41 +0000

    Start from scratch: first test

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,3 @@
-class Account
-
-  attr_reader :balance
-
-  def initialize
-    @balance = 0.00 
-  end
-
-  def first_deposit
-    1000.00
-  end
-  
+def deposit_display(amount)
+  1000.00
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,21 +1,11 @@
 require 'account'
 
-describe 'Account' do
-  
-  account = Account.new
-
-  describe '#balance' do
-    it 'opens up a bank account with zero balance' do
-      expect(account.balance).to eq 0.00
-    end
-  end
-
-  describe '#first_deposit' do
-    it 'makes a deposit of 1000 on 10-01-2012' do
-      expect(account.first_deposit).to eq 1000.00 
-    end
+describe '#deposit_display' do
+  it 'displays a given amount in the correct format' do
+    expect(deposit_display(1000)).to eq 1000.00
   end
 end
+
 
 
 
